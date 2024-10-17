0c14154544572960f8eaf91f0183cae8ef208d59
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-10-17 15:56:49 +0900

    [Silver IV] Title: 듣보잡, Time: 28 ms, Memory: 9912 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -13,41 +13,28 @@ int main() {
 
     vector<string> listen(n);
     vector<string> see(m);
-    vector<string> result; // 듣보잡의 결과를 저장할 벡터
+    vector<string> result;
 
-    // 듣도 못한 사람들 입력
     for (int i = 0; i < n; ++i) {
         cin >> listen[i];
     }
 
-    // 보도 못한 사람들 입력
     for (int i = 0; i < m; ++i) {
         cin >> see[i];
     }
 
-    // 듣도 못한 사람들 정렬
-    sort(listen.begin(), listen.end());
-    // 보도 못한 사람들 정렬
     sort(see.begin(), see.end());
 
-    // 교집합 찾기
-    int i = 0, j = 0;
-    while (i < n && j < m) {
-        if (listen[i] < see[j]) {
-            ++i; // 듣도 못한 사람의 이름이 더 작으면 다음으로
-        } else if (listen[i] > see[j]) {
-            ++j; // 보도 못한 사람의 이름이 더 작으면 다음으로
-        } else {
-            result.push_back(listen[i]); // 두 이름이 같으면 결과에 추가
-            ++i; // 다음으로
-            ++j; // 다음으로
+    for (const auto& name : listen) {
+        if (binary_search(see.begin(), see.end(), name)) {
+            result.push_back(name);
         }
     }
 
-    // 결과 출력
-    cout << result.size() << "\n"; // 교집합의 크기 출력
+    cout << result.size() << "\n";
+    sort(result.begin(), result.end());
     for (const auto& name : result) {
-        cout << name << "\n"; // 교집합의 이름 출력
+        cout << name << "\n"; 
     }
 
     return 0;
