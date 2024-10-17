5c288592df398072b73ffb006920415f159083ba
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-10-17 18:31:34 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-long long Factorial(int n)
+long long Factorial(long long n)
 {
-    if (n == 0)
-    {
-        return 1;
-    }
+    if (n == 0) return 1;
     return n * Factorial(n - 1);
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n;
     cin >> n;
 
-    long long answer = Factorial(n);
-    cout << answer;
+    cout << Factorial(n);
 }
