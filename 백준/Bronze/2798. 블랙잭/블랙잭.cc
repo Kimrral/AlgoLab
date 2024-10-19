b68eb9a3180a11ae92ff1449258919c730250af5
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-10-19 18:41:07 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,28 +1,27 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 int main(){
     int n, m;
     cin>>n>>m;
     
+    int maxsum = 0;
     vector<int> v(n);
     for(int i=0; i<n; ++i){
         cin>>v[i];
     }
     
-    int maxsum = 0;
-    
-    for(int i =0; i<n-2; ++i){
-        for(int j = i+1; j<n-1; ++j){
-            for(int k = j+1; k<n; ++k){
-                int sum = v[i]+v[j]+v[k];
-                if(sum<=m && sum>maxsum){
-                    maxsum = sum;
-                }                
+    for(int i=0; i<n-2; ++i){
+        for(int j=i+1; j<n-1; ++j){
+            for(int k=j+1; k<n; ++k){
+                int sum = v[i]+v[j]+v[k];                
+                if(sum<=m){
+                    maxsum = max(maxsum, sum);
                 }
             }
         }
+    }
     
-    
-    cout<<maxsum;
+    cout << maxsum;
 }
