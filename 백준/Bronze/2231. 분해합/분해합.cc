3608474c815b23a60315e0f6ca22dc1219a1f511
Kimral <121008986+Kimrral@users.noreply.github.com>
2024-10-19 18:58:27 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,30 +1,26 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+int getDigits(int num){
+    int sum = 0;
+    while(num!=0){
+        sum += num%10;
+        num/=10;
+    }
+    return sum;
+}
 int main(){
     int n;
     cin>>n;
     
-    int minsum = 0;
-    
-    for(int i=1; i<=n; ++i){
-            int num = i;
-            int sum = 0;
-            while(num!=0){
-                sum += num%10;
-                num/=10;
-            }
-        
-            if(sum+i==n){
-                minsum = i;
-                break;
-            }
+    int i = 1;
+    while(i<n){
+        if(getDigits(i)+i == n){
+            cout << i;
+            return 0;
+        }
+        i++;
     }
     
-    if(minsum==0){
-        cout<<0;
-        return 0;
-    }
-    
-    cout<<minsum;
+    cout << 0;
+    return 0;
 }
